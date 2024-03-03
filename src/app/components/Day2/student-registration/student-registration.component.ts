8a0e019e339b30b01d1a1bbbb49a0e71dbffa505
Noor285 <nm1701037@gmail.com>
2024-03-03 15:40:24 +0200

    day2changes

--- src/app/components/Day2/student-registration/student-registration.component.ts
+++ src/app/components/Day2/student-registration/student-registration.component.ts
@@ -1,38 +1,56 @@
 import { Component } from '@angular/core';
 
-interface Student {
-  name: string;
-  age: number;
-  address: string;
-  phone: string;
-}
-
 @Component({
   selector: 'app-student-registration',
   templateUrl: './student-registration.component.html',
   styleUrls: ['./student-registration.component.css']
 })
 export class StudentRegistrationComponent {
-  student: Student = {
-    name: '',
-    age: 10,
-    address: '',
-    phone: ''
-  };
-  registeredStudents: Student[] = [];
 
+  Students: any[] = [];
 
-  onSubmit() {
-    if (this.student.name && this.student.age && this.student.address && this.student.phone) {
-      this.registeredStudents.push({ ...this.student });
-      this.student.name = '';
-      this.student.age = 10;
-      this.student.address = '';
-      this.student.phone = '';
-    }
+  name : string = "";
+  age : number = 0;
+  phone : string = "";
+  address : string = "";
+
+  sendValues(){
+    if (this.NameValidation && this.AgeValidation && this.address && this.PhoneValidation) {
+      this.Students.push({ name: this.name, age: this.age, phone: this.phone, address: this.address });
+
+      this.name = '';
+      this.age = 0;
+      this.phone = '';
+      this.address = '';
+      }
+  }
+
+  get NameValidation(){
+    return this.name.length >=4;
+  }
+
+  get NameTouched(){
+    return this.name != "";
+  }
+
+  get AgeValidation(){
+    return this.age >= 10 && this.age <= 17
+  }
+
+  get AgeTouched(){
+    return this.age != 0;
+  }
+
+  get PhoneValidation(){
+    const regex = /^(010|011|012|015)\d{8}$/;
+    return regex.test(this.phone);
+  }
+
+get PhoneTouched(){
+    return this.phone != "";
   }
 
   removeStudent(index: number) {
-    this.registeredStudents.splice(index, 1);
+    this.Students.splice(index, 1);
   }
 }
